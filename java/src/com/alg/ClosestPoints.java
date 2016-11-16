c32e0f196fa42c3f6f8c1b97db0366eec56876eb
Rupendra Bandyopadhyay <rbandyo@yahoo.com>
2016-11-16 15:10:58 -0500

    Update ClosestPoints.java

--- java/src/com/alg/ClosestPoints.java
+++ java/src/com/alg/ClosestPoints.java
@@ -2,6 +2,7 @@ package com.alg;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Comparator;
 import java.util.Random;
 
@@ -10,10 +11,32 @@ public class ClosestPoints
     Point[] points;
     Random rand = new Random();
 
+    private void addPosition()
+    {
+        if (points == null)
+        {
+            return;
+        }
+        for (int i=0; i<points.length; i++)
+        {
+            points[i].originalPosition = i;
+        }
+    }
     public ClosestPoints(Point[] points)
     {
         super();
         this.points = points;
+        addPosition();
+    }
+    
+    public ClosestPoints(double[][] data)
+    {
+        points = new Point[data.length];
+        for (int i=0; i<data.length; i++)
+        {
+            points[i] = new Point(data[i][0], data[i][1]);
+        }
+        addPosition();
     }
 
     Point getRandomPoint(double minX, double maxX, double minY, double maxY)
@@ -28,6 +51,7 @@ public class ClosestPoints
         {
             points[i] = getRandomPoint(minX, maxX, minY, maxY);
         }
+        addPosition();
     }
     
     public Point[] sortedByX()
@@ -35,7 +59,7 @@ public class ClosestPoints
         Point[] pointsX = new Point[points.length];
         for (int i=0; i<points.length; i++)
         {
-            pointsX[i] = new Point(points[i].x, points[i].y);
+            pointsX[i] = new Point(points[i].x, points[i].y, points[i].originalPosition);
         }
         Arrays.sort(pointsX, new Comparator<Point>() {
             @Override
@@ -63,7 +87,7 @@ public class ClosestPoints
         Point[] pointsY = new Point[points.length];
         for (int i=0; i<points.length; i++)
         {
-            pointsY[i] = new Point(points[i].x, points[i].y);
+            pointsY[i] = new Point(points[i].x, points[i].y, points[i].originalPosition);
         }
         Arrays.sort(pointsY, new Comparator<Point>() {
             @Override
@@ -85,14 +109,72 @@ public class ClosestPoints
         });
         return pointsY;
     }
+    
+    public void printPointsWithPosition(Point[] data)
+    {
+        printPointsWithPosition(data, null);
+    }
+    
+    public void printPointsWithPosition(Point[] data, String prefix)
+    {
+        StringBuilder sb = new StringBuilder();
+        if (prefix != null)
+        {
+            sb.append(prefix + ": ");
+        }
+        if (data != null && data.length > 0)
+        {
+            sb.append("{");
+            int i = 0;
+            for (Point p: data)
+            {
+                if (i > 0)
+                {
+                    sb.append(", ");
+                }
+                sb.append("{");
+                sb.append(p.x + ", " + p.y + " : " + p.originalPosition);
+                sb.append("}");
+                i++;
+            }
+            sb.append("}");
+        }
+        System.out.println(sb.toString());
+    }
+    
+    public void getLeftAndRightSortedArrays(Point[] sortedPoints, Point[] sortedLeft, Point[] sortedRight, int from, int to)
+    {
+        // System.out.println("In getLeftAndRightSortedArrays");
+        // printPointsWithPosition(sortedPoints);
+        // System.out.println(sortedLeft.length + " : " + sortedRight.length);
+        int leftPos = 0;
+        int rightPos = 0;
+        int mid = (from + to) / 2;
+        for (Point p : sortedPoints)
+        {
+            if (p.originalPosition < mid)
+            {
+                Point np = new Point(p.x, p.y, p.originalPosition);
+                sortedLeft[leftPos++] = np;
+            }
+            else
+            {
+                Point np = new Point(p.x, p.y, p.originalPosition);
+                sortedRight[rightPos++] = np;
+            }
+        }
+    }
 
     public Point[] closestPair()
     {
+        // printPointsWithPosition(points, "P");
         Point[] Px = sortedByX();
+        points = Px;
+        addPosition();
         Point[] Py = sortedByY();
-        int from = 0;
-        int to = points.length;
-        return closestPair(Px, Py, from, to);
+        // System.out.println("Px = " + Arrays.toString(Px));
+        // System.out.println("Py = " + Arrays.toString(Py));
+        return closestPair(Px, Py, 0, Px.length);
     }
 
     public Point[] closer(Point[] set1, Point[] set2)
@@ -117,35 +199,78 @@ public class ClosestPoints
         }
     }
     
-    public Point[] closestPair(Point[] Px, Point[] Py, int from, int to)
+    public Point[] closestPairBF(Point[] Px)
     {
-        Point[] result = new Point[2];
-        // Base case; length of both arrays = 1
-        if (to - from == 1) // Only one point, no pair possible
+        if (Px.length <= 1)
         {
             return null;
         }
-        if (to - from == 2) // Return the only pair
+        double minDistance = -1;
+        Point[] closestPoints = new Point[2];
+        for (int i=0; i<Px.length; i++)
         {
-            result[0] = Px[from];
-            result[1] = Py[from+1];
-            return result;
+            for (int j=i+1; j<Px.length; j++)
+            {
+                double distance = Px[i].distance(Px[j]);
+                if (minDistance == -1)
+                {
+                    minDistance  = distance;
+                    closestPoints[0] = Px[i];
+                    closestPoints[1] = Px[j];
+                }
+                else if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    closestPoints[0] = Px[i];
+                    closestPoints[1] = Px[j];
+                }
+            }
         }
-        // There are at least three points in the set
-        int mid = (from + to) / 2;
-        Point[] resultQ = closestPair(Px, Py, from, mid);
-        Point[] resultR = closestPair(Px, Py, mid, to);
+        // printPointsWithPosition(closestPoints, "closest");
+        // System.out.println("Distance = " + closestPoints[0].distance(closestPoints[1]));
+        return closestPoints;
+    }
+    
+    public Point[] closestPair(Point[] Px, Point[] Py, int from, int to)
+    {
+        // printPointsWithPosition(Px, "Px");
+        // printPointsWithPosition(Py, "Py");
+        // Base case; length of both arrays = 1
+        if (Px.length <= 3) // # 3 or less, use brute force
+        {
+            return closestPairBF(Px);
+        }
+        // There are at four or more points in each set
+        int mid = Px.length / 2;
+        Point[] Qx = new Point[mid];
+        Point[] Rx = new Point[Px.length - mid];
+        Point[] Qy = new Point[mid];
+        Point[] Ry = new Point[Px.length - mid];
+        getLeftAndRightSortedArrays(Px, Qx, Rx, from, to);
+        getLeftAndRightSortedArrays(Py, Qy, Ry, from, to);
+        // printPointsWithPosition(Qx, "Qx");
+        // printPointsWithPosition(Qy, "Qy");
+        // printPointsWithPosition(Rx, "Rx");
+        // printPointsWithPosition(Ry, "Ry");
+        int newMid = (from + to) / 2;
+        Point[] resultQ = closestPair(Qx, Qy, from, newMid);
+        Point[] resultR = closestPair(Rx, Ry, newMid, to);
         double distQ = resultQ[0].distance(resultQ[1]);
         double distR = resultR[0].distance(resultR[1]);
         double delta = Math.min(distQ, distR);
-        Point[] resultS = closestSplitPair(Px, Py, from, to, delta);
+        Point[] resultS = closestSplitPair(Px, Py, delta);
         return closer(closer(resultQ, resultR), resultS);
     }
     
-    public Point[] closestSplitPair(Point[] Px, Point[] Py, int from, int to, double delta)
+    public Point[] closestSplitPair(Point[] Px, Point[] Py, double delta)
     {
-        int mid = (from + to) / 2;
-        double xBar = Px[mid].x;
+        // System.out.println("In closestSplitPair");
+        // printPointsWithPosition(Px, "Px");
+        // printPointsWithPosition(Py, "Py");
+        // System.out.println("delta = " + delta);
+        int mid = Px.length / 2;
+        double xBar = Px[mid-1].x;
+        // System.out.println("xBar = " + xBar);
         ArrayList<Point> Sy = new ArrayList<Point>();
         for (Point p : Py)
         {
@@ -154,7 +279,26 @@ public class ClosestPoints
                 Sy.add(p);
             }
         }
-        return null;
+        // System.out.println("Size of Sy = " + Sy.size());
+        Point[] resultS = null;
+        double minDistance = delta;
+        for (int i=0; i<Sy.size(); i++)
+        {
+            Point p1 = Sy.get(i);
+            int remaining = Sy.size() - i;
+            remaining = Math.min(remaining, 7);
+            for (int j=1; j<remaining; j++)
+            {
+                Point p2 = Sy.get(i+j);
+                double distance = p1.distance(p2);
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    resultS = new Point[]{p1,p2};
+                }
+            }
+        }
+        return resultS;
     }
     
     public int[] computeClosestBruteForce()
@@ -190,6 +334,7 @@ public class ClosestPoints
     {
         double x;
         double y;
+        int originalPosition;
 
         public Point()
         {
@@ -203,6 +348,14 @@ public class ClosestPoints
             this.y = y;
         }
 
+        public Point(double x, double y, int originalPosition)
+        {
+            super();
+            this.x = x;
+            this.y = y;
+            this.originalPosition = originalPosition;
+        }
+        
         public double distance(Point other)
         {
             double distance = Math.pow(Math.pow((other.x - x), 2) + Math.pow((other.y - y), 2), 0.5);
@@ -212,7 +365,7 @@ public class ClosestPoints
         @Override
         public String toString()
         {
-            return String.format("[%.2f,%.2f]", x, y);
+            return String.format("{%.2f,%.2f}", x, y);
         }
     }
 
@@ -224,23 +377,71 @@ public class ClosestPoints
         }
     }
     
+    public static void testRandom()
+    {
+        ClosestPoints cp = new ClosestPoints(10000, 0, 10000, 0, 10000);
+        if (cp.points.length <= 100)
+        {
+            System.out.println(Arrays.toString(cp.points));
+        }
+        if (! test_cp(cp))
+        {
+            System.out.println("Failed");
+        }
+    }
+    
+    public static boolean test_cp(ClosestPoints cp)
+    {
+        long start = Calendar.getInstance().getTimeInMillis();
+        int[] closest = cp.computeClosestBruteForce();
+        long end = Calendar.getInstance().getTimeInMillis();
+        long time1 = end - start;
+        System.out.println(
+                String.format("Closest %s to %s ; distance = %.2f", cp.getAt(closest[0]), cp.getAt(closest[1]),
+                        cp.getAt(closest[0]).distance(cp.getAt(closest[1]))));
+        double dist1 = cp.getAt(closest[0]).distance(cp.getAt(closest[1]));
+        System.out.println(String.format("Time Brute force = %d ms", time1));
+        
+        start = Calendar.getInstance().getTimeInMillis();
+        Point[] closestPoints = cp.closestPair();
+        end = Calendar.getInstance().getTimeInMillis();
+        long time2 = end - start;
+        if (closestPoints != null && closestPoints.length == 2)
+        {
+            System.out.println(String.format("Closest %s to %s ; distance = %.2f", closestPoints[0], closestPoints[1],
+                    closestPoints[0].distance(closestPoints[1])));
+        }
+        double dist2 = closestPoints[0].distance(closestPoints[1]);
+        // System.out.println(dist1 * 100 + ": " + dist2 * 100);
+        System.out.println(String.format("Time Divide and Conquer = %d ms", time2));
+        return (int) (dist1 * 100) == (int) (dist2 * 100);
+    }
+    
+    public static void test_specific_01()
+    {
+        double[][] data = {{7.79,0.89}, {8.35,1.75}, {0.92,5.28}, {0.32,5.20}, {8.64,1.72}, {4.82,0.93}, {6.66,9.30}, {3.12,9.13}, {8.94,3.95}, {2.87,4.85}};
+        ClosestPoints cp = new ClosestPoints(data);
+        test_cp(cp);
+    }
+    
+    public static void test_specific_02()
+    {
+        double[][] data = {{2.29,3.68}, {6.05,5.87}, {3.15,6.64}, {1.79,5.91}, {8.86,3.85}, {8.13,9.87}, {4.51,2.18}, {0.93,3.13}, {2.80,5.23}, {9.71,4.23}};
+        ClosestPoints cp = new ClosestPoints(data);
+        test_cp(cp);
+    }
+
+    public static void test_specific_03()
+    {
+        double[][] data = {{0.04,4.06}, {4.09,5.87}, {5.30,3.99}, {6.10,6.65}, {2.97,5.66}, {9.07,6.98}, {3.42,5.84}, {1.25,8.75}, {4.93,3.00}, {9.41,4.58}};
+        ClosestPoints cp = new ClosestPoints(data);
+        test_cp(cp);
+    }
+
     public static void main(String[] args)
     {
-        ClosestPoints cp = new ClosestPoints(10, 0, 10, 0, 10);
-        int[] closest = cp.computeClosestBruteForce();
-        System.out.println(
-                String.format("Closest %d %s to %d %s ; distance = %.2f", closest[0], cp.getAt(closest[0]), 
-                        closest[1], cp.getAt(closest[1]),
-                        cp.getAt(closest[0]).distance(cp.getAt(closest[1]))));
-        System.out.println("Original");
-        System.out.println("--------");
-        printPoints(cp.points);
-        System.out.println("P x");
-        System.out.println("--------");
-        printPoints(cp.sortedByX());
-        System.out.println("P y");
-        System.out.println("--------");
-        printPoints(cp.sortedByY());
+        // test_specific_03();
+        testRandom();
     }
 
 }
