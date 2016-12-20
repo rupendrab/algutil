3aa1b83b162ee7ad746cff3ec0774346217b2470
Rupendra Bandyopadhyay <rbandyo@yahoo.com>
2016-12-20 15:14:26 -0500

    Update RunningMedian.java

--- java/src/com/alg/datastructures/RunningMedian.java
+++ java/src/com/alg/datastructures/RunningMedian.java
@@ -1,10 +1,17 @@
 package com.alg.datastructures;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Random;
+
+import com.alg.graph.bst.BinarySearchTree;
 
 public class RunningMedian<T extends Comparable<? super T>>
 {
@@ -95,9 +102,78 @@ public class RunningMedian<T extends Comparable<? super T>>
         }
     }
     
-    public static void main(String[] args)
+    public static long time()
     {
-        test01();
+        return Calendar.getInstance().getTimeInMillis();
+    }
+    
+    public static int test02(String fileName) throws IOException
+    {
+        long start, end = 0;
+        start = time();
+        RunningMedian<Integer> rm = new RunningMedian<>(new Integer[] {});
+        BufferedReader reader = new BufferedReader(new FileReader(new File(fileName)));
+        String line = null;
+        int lineNo = 0;
+        long sumOfMedians = 0;
+        while((line = reader.readLine()) != null)
+        {
+            lineNo++;
+            try
+            {
+                Integer val = Integer.parseInt(line);
+                rm.addItem(val);
+                ArrayList<Integer> medians = rm.median();
+                Integer median = medians.get(0);
+                sumOfMedians += median;
+            }
+            catch (NumberFormatException ne)
+            {
+                System.err.println(String.format("Invalid Integer at line %d = %s", lineNo, line));
+            }
+        }
+        reader.close();
+        end = time();
+        System.out.println(String.format("Time elapsed in test02 = %d ms", (end - start)));
+        return (int) (sumOfMedians % 10000);
+    }
+    
+    public static int test03(String fileName) throws IOException
+    {
+        long start, end = 0;
+        start = time();
+        BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>(null);
+        BufferedReader reader = new BufferedReader(new FileReader(new File(fileName)));
+        String line = null;
+        int lineNo = 0;
+        long sumOfMedians = 0;
+        while((line = reader.readLine()) != null)
+        {
+            lineNo++;
+            try
+            {
+                Integer val = Integer.parseInt(line);
+                bst.insert(val);
+                Integer median = bst.median();
+                sumOfMedians += median;
+            }
+            catch (NumberFormatException ne)
+            {
+                System.err.println(String.format("Invalid Integer at line %d = %s", lineNo, line));
+            }
+        }
+        reader.close();
+        end = time();
+        System.out.println(String.format("Time elapsed in test03 = %d ms", (end - start)));
+        System.out.println(bst.computeHeight());
+        return (int) (sumOfMedians % 10000);
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        // test01();
+        System.out.println(test02("C:\\Users\\rubandyopadhyay\\Downloads\\Median.txt"));
+        System.out.println(test03("C:\\Users\\rubandyopadhyay\\Downloads\\Median.txt"));
     }
     
 }
