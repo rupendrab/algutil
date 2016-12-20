36f091a4cde6ef5ceb51ccbedab78c15c5cb7056
Rupendra Bandyopadhyay <rbandyo@yahoo.com>
2016-12-20 15:15:51 -0500

    Update BinarySearchTree.java

--- java/src/com/alg/graph/bst/BinarySearchTree.java
+++ java/src/com/alg/graph/bst/BinarySearchTree.java
@@ -1,5 +1,8 @@
 package com.alg.graph.bst;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class BinarySearchTree<T extends Comparable<? super T>>
 {
     T root;
@@ -12,7 +15,10 @@ public class BinarySearchTree<T extends Comparable<? super T>>
     {
         super();
         this.root = root;
-        this.size = 1;
+        if (root != null)
+        {
+            this.size = 1;
+        }
     }
 
     public BinarySearchTree(T root, BinarySearchTree<T> left, BinarySearchTree<T> right)
@@ -30,6 +36,36 @@ public class BinarySearchTree<T extends Comparable<? super T>>
             right.setParent(this);
         }
         computeSize();
+    }
+    
+    public BinarySearchTree(T[] arr, int start, int end, boolean sort)
+    {
+        if (arr == null)
+        {
+            return;
+        }
+        if (start == end - 1)
+        {
+            root = arr[start];
+            size = 1;
+            return;
+        }
+        if (sort)
+        {
+            Arrays.sort(arr);
+        }
+        int midPosition = start + (end - start) / 2;
+        // Forward midPosition until the same value persists, ensuring equal values go to left subtree
+        int i = midPosition;
+        for (i=midPosition; i<end-1 && compareTo(arr[i+1], arr[midPosition]) == 0; i++);
+        midPosition = i;
+        root = arr[midPosition];
+        size = end - start;
+        left = new BinarySearchTree<T>(arr, start, midPosition, false);
+        if (midPosition < end -1)
+        {
+            right = new BinarySearchTree<T>(arr, midPosition+1, end, false);
+        }
     }
     
     public int compareTo(T a, T b)
@@ -92,6 +128,12 @@ public class BinarySearchTree<T extends Comparable<? super T>>
      */
     public void insert(T elem)
     {
+        if (root == null)
+        {
+            root = elem;
+            size = 1;
+            return;
+        }
         int cmp = compareTo(elem, root);
         if (cmp <= 0) // If less or equal, choose the left subtree
         {
@@ -160,7 +202,11 @@ public class BinarySearchTree<T extends Comparable<? super T>>
             {
                 bst = bst.parent;
             }
-            if (compareTo(bst.root, bstElem.root) > 0)
+            if (bst == null)
+            {
+                return null;
+            }
+            else if (compareTo(bst.root, bstElem.root) > 0)
             {
                 return null;
             }
@@ -171,6 +217,197 @@ public class BinarySearchTree<T extends Comparable<? super T>>
         }
     }
     
+    public BinarySearchTree<T> predecessor(BinarySearchTree<T> reference)
+    {
+        if (reference == null)
+        {
+            return null;
+        }
+        if (reference.left != null)
+        {
+            return reference.left.max();
+        }
+        else
+        {
+            BinarySearchTree<T> bst = reference.parent;
+            while (bst != null && compareTo(bst.root, reference.root) > 0)
+            {
+                bst = bst.parent;
+            }
+            if (bst == null)
+            {
+                return null;
+            }
+            else if (compareTo(bst.root, reference.root) > 0)
+            {
+                return null;
+            }
+            else
+            {
+                return bst;
+            }
+        }
+    }
+
+    public BinarySearchTree<T> successor(T elem)
+    {
+        // Not Implemented
+        BinarySearchTree<T> bstElem = locate(elem);
+        if (bstElem == null)
+        {
+            return null;
+        }
+        if (bstElem.right != null)
+        {
+            return bstElem.right.min();
+        }
+        else
+        {
+            BinarySearchTree<T> bst = bstElem.parent;
+            while (bst != null && compareTo(bst.root, bstElem.root) < 0)
+            {
+                bst = bst.parent;
+            }
+            if (bst == null)
+            {
+                return null;
+            }
+            else if (compareTo(bst.root, bstElem.root) < 0)
+            {
+                return null;
+            }
+            else
+            {
+                return bst;
+            }
+        }
+    }
+    
+    public BinarySearchTree<T> select(int pos)
+    {
+        // Returns the element at indicated position. Position is zero based
+        if (pos < 0 || pos > size - 1)
+        {
+            return null;
+        }
+        int currentPosition = 0;
+        if (left != null)
+        {
+            currentPosition = left.size;
+        }
+        if (currentPosition == pos)
+        {
+            return this;
+        }
+        else if (pos < currentPosition)
+        {
+            return left.select(pos);
+        }
+        else
+        {
+            if (right == null)
+            {
+                return null;
+            }
+            else
+            {
+                return right.select(pos - currentPosition - 1);
+            }
+        }
+    }
+    
+    public BinarySearchTree<T> medianTree()
+    {
+        if (size == 0)
+        {
+            return null;
+        }
+        int pos = size / 2;
+        if (size % 2 == 0)
+        {
+            pos -= 1;
+        }
+        return select(pos);
+    }
+    
+    public T median()
+    {
+        BinarySearchTree<T> mTree = medianTree();
+        if (mTree == null)
+        {
+            return null;
+        }
+        return mTree.root;
+    }
+
+    public int rank(T elem, int startRank)
+    {
+        // Returns the zero based rank of an element
+        int currentPosition = startRank;
+        if (left != null)
+        {
+            currentPosition += left.size;
+        }
+        int cmp = compareTo(elem, root);
+        if (cmp == 0)
+        {
+            if (left != null)
+            {
+                int posInLeftTree = left.rank(elem, startRank);
+                return posInLeftTree != -1 ? posInLeftTree : currentPosition;
+            }
+            else
+            {
+                return currentPosition;
+            }
+        }
+        else if (cmp < 0)
+        {
+            if (left == null)
+            {
+                return -1;
+            }
+            else
+            {
+                return left.rank(elem, startRank);
+            }
+        }
+        else
+        {
+            if (right == null)
+            {
+                return -1;
+            }
+            else
+            {
+                return right.rank(elem, currentPosition + 1);
+            }
+        }
+    }
+    
+    public int rank(T elem)
+    {
+        return rank(elem, 0);
+    }
+    
+    public ArrayList<T> orderedArray()
+    {
+        ArrayList<T> arr = new ArrayList<>();
+        if (left != null)
+        {
+            arr.addAll(left.orderedArray());
+        }
+        if (root != null)
+        {
+            arr.add(root);
+        }
+        if (right != null)
+        {
+            arr.addAll(right.orderedArray());
+        }
+        return arr;
+    }
+    
     public int delete(T elem)
     {
         BinarySearchTree<T> bst = locate(elem);
@@ -178,29 +415,89 @@ public class BinarySearchTree<T extends Comparable<? super T>>
         {
             return 0;
         }
+        boolean hasParent = bst.parent != null;
+        boolean isLeftOfParent = hasParent && compareTo(bst.root, bst.parent.root) <= 0;
         if (bst.left == null && bst.right == null)
         {
             // No left or right child
-            boolean isLeftOfParent = compareTo(bst.root, bst.parent.root) <= 0;
-            if (isLeftOfParent)
+            if (hasParent)
             {
-                bst.parent.setLeft(null);
+                if (isLeftOfParent)
+                {
+                    bst.parent.setLeft(null);
+                }
+                else
+                {
+                    bst.parent.setRight(null);
+                }
+                bst.parent.computeSizeAllUpwards();
             }
             else
             {
-                bst.parent.setRight(null);
+                // Last Node deleted (Special case)
+                root = null;
+                left = null;
+                right = null;
+                size = 0;
             }
-            bst = null;
         }
         else if (bst.left != null && bst.right != null)
         {
             // Both left and right child
+            BinarySearchTree<T> pred = predecessor(bst);
+            BinarySearchTree<T> predParent = pred.parent;
+            if (predParent.root.compareTo(pred.root) >= 0)
+            {
+                predParent.setLeft(pred.left);
+            }
+            else
+            {
+                predParent.setRight(pred.left);
+            }
+            bst.setRoot(pred.getRoot());
+            predParent.computeSizeAllUpwards();
         }
         else
         {
             // Either a left or a right child
+            BinarySearchTree<T> child = bst.left == null ? bst.right : bst.left;
+            if (bst.parent != null)
+            {
+                if (isLeftOfParent)
+                {
+                    bst.parent.setLeft(child);
+                }
+                else
+                {
+                    bst.parent.setRight(child);
+                }
+                bst.parent.computeSizeAllUpwards();
+            }
+            else
+            {
+                child.parent = null;
+                root = child.root;
+                setLeft(child.left);
+                setRight(child.right);
+                this.computeSizeAllUpwards();
+            }
         }
         return 1;
+    }
+    
+    public int computeHeight()
+    {
+        int leftHeight = 0;
+        int rightHeight = 0;
+        if (left != null)
+        {
+            leftHeight = left.computeHeight();
+        }
+        if (right != null)
+        {
+            rightHeight = right.computeHeight();
+        }
+        return Math.max(leftHeight, rightHeight) + 1;
     }
     
     @Override
@@ -213,7 +510,7 @@ public class BinarySearchTree<T extends Comparable<? super T>>
             sb.append(left.toString());
         }
         sb.append(":");
-        sb.append(root.toString() + "(" + size + ")");
+        sb.append((root == null ? "" : root.toString()) + "(" + size + ")");
         sb.append(":");
         if (right != null)
         {
@@ -276,6 +573,97 @@ public class BinarySearchTree<T extends Comparable<? super T>>
     private void computeSize()
     {
         this.size = 1 + (this.left == null ? 0 : this.left.size) + (this.right == null ? 0 : this.right.size);
+    }
+
+    private void computeSizeAllUpwards()
+    {
+        computeSize();
+        BinarySearchTree<T> p = this.parent;
+        while (p != null)
+        {
+            p.computeSize();
+            p = p.parent;
+        }
+    }
+    
+    public void destroy()
+    {
+    }
+    
+    public void unlink()
+    {
+        boolean hasParent = parent != null;
+        boolean isLeftOfParent = hasParent && compareTo(root, parent.root) <= 0;
+        if (hasParent)
+        {
+            if (isLeftOfParent)
+            {
+                parent.left = null;
+            }
+            else
+            {
+                parent.right = null;
+            }
+        }
+    }
+    
+    public int getLeftSize()
+    {
+        return left == null ? 0 : left.size;
+    }
+    
+    public int getRightSize()
+    {
+        return right == null ? 0 : right.size;
+    }
+
+    public boolean checkTree()
+    {
+        if (root == null && size == 0 && getLeftSize() == 0 && getRightSize() == 0)
+        {
+            return true;
+        }
+        if (size != getLeftSize() + 1 + getRightSize())
+        {
+            // System.out.println("Failed Size check at " + root);
+            // System.out.println(String.format("Size = %d, left = %d, right = %d", getLeftSize(), getRightSize()));
+            return false;
+        }
+        if (left != null)
+        {
+            if (compareTo(left.root, root) > 0)
+            {
+                // System.out.println(String.format("Value = %s, left = %s", root, left.root));
+                return false;
+            }
+            if (this != left.parent)
+            {
+                System.out.println(String.format("Left Child mismatch: Value = %s, left = %s", root, left.root));
+                return false;
+            }
+            if (! left.checkTree())
+            {
+                return false;
+            }
+        }
+        if (right != null)
+        {
+            if (compareTo(right.root, root) <= 0)
+            {
+                // System.out.println(String.format("Value = %s, right = %s", root, right.root));
+                return false;
+            }
+            if (this != right.parent)
+            {
+                System.out.println(String.format("Right Child mismatch: Value = %s, left = %s", root, right.root));
+                return false;
+            }
+            if (! right.checkTree())
+            {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
