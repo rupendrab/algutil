d6acfe14038f9294282a9b7172b96d53fb506ea4
Rupendra Bandyopadhyay <rbandyo@yahoo.com>
2016-12-21 16:00:11 -0500

    Create TestRBTree.java

---
+++ java/src/com/alg/graph/bst/TestRBTree.java
@@ -0,0 +1,22 @@
+package com.alg.graph.bst;
+
+public class TestRBTree
+{
+    public static void test01()
+    {
+        RedBlackTree<Integer> rbt = new RedBlackTree<Integer>(null);
+        for (int i=0; i<3; i++)
+        {
+            rbt.insert(i);
+            System.out.println(rbt);
+        }
+        System.out.println(rbt.computeHeight());
+        System.out.println(rbt.size);
+    }
+    
+    public static void main(String[] args) throws Exception
+    {
+        test01();
+    }
+
+}
