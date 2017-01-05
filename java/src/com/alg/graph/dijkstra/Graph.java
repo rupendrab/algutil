1e5de00308638818fa857de5808b3e84825c1f66
Rupendra Bandyopadhyay <rbandyo@yahoo.com>
2017-01-05 16:44:21 -0500

    Update Graph.java

--- java/src/com/alg/graph/dijkstra/Graph.java
+++ java/src/com/alg/graph/dijkstra/Graph.java
@@ -28,13 +28,77 @@ public class Graph
         return nodesEdges.get(node);
     }
     
+    public HashSet<Integer> getNodes()
+    {
+        return nodes;
+    }
+
+    public void setNodes(HashSet<Integer> nodes)
+    {
+        this.nodes = nodes;
+    }
+
+    public HashMap<Integer, ArrayList<Edge>> getNodesEdges()
+    {
+        return nodesEdges;
+    }
+
+    public void setNodesEdges(HashMap<Integer, ArrayList<Edge>> nodesEdges)
+    {
+        this.nodesEdges = nodesEdges;
+    }
+
+    public void addEdge(Integer node, Edge e)
+    {
+        ArrayList<Edge> edges = getEdges(node);
+        if (edges == null)
+        {
+            edges = new ArrayList<Edge>();
+            nodesEdges.put(node, edges);
+        }
+        edges.add(e);
+    }
+    
+    public void addEdge(Edge e)
+    {
+        nodes.add(e.getFrom());
+        nodes.add(e.getTo());
+        addEdge(e.getFrom(), e);
+        addEdge(e.getTo(), e);
+    }
+    
+    public long sumEdgeLengths()
+    {
+        long sumLengths = 0;
+        for (Entry<Integer, ArrayList<Edge>> entry : nodesEdges.entrySet())
+        {
+            ArrayList<Edge> edges = entry.getValue();
+            for (Edge edge : edges)
+            {
+                sumLengths += edge.distance;
+            }
+        }
+        return sumLengths / 2;
+    }
+    
     public static Graph readFromFile(String fileName) throws IOException
+    {
+        return readFromFile(fileName, 0);
+    }
+    
+    public static Graph readFromFile(String fileName, int ignorelines) throws IOException
     {
         Graph g = new Graph();
         BufferedReader reader = new BufferedReader(new FileReader(new File(fileName)));
         String line = null;
+        int lineNo = 0;
         while ((line = reader.readLine()) != null)
         {
+            lineNo++;
+            if (lineNo <= ignorelines)
+            {
+                continue;
+            }
             line = line.trim();
             if (line != null && line.length() > 0)
             {
@@ -67,6 +131,37 @@ public class Graph
         return g;
     }
     
+    public static Graph readFromFileFormatTwo(String fileName, int ignorelines) throws IOException
+    {
+        Graph g = new Graph();
+        BufferedReader reader = new BufferedReader(new FileReader(new File(fileName)));
+        String line = null;
+        int lineNo = 0;
+        while ((line = reader.readLine()) != null)
+        {
+            lineNo++;
+            if (lineNo <= ignorelines)
+            {
+                continue;
+            }
+            line = line.trim();
+            if (line != null && line.length() > 0)
+            {
+                String[] parts = line.split("\\s+");
+                if (parts.length == 3)
+                {
+                    int startNode = Integer.parseInt(parts[0]);
+                    int nextNode = Integer.parseInt(parts[1]);
+                    int distance = Integer.parseInt(parts[2]);
+                    Edge e = new Edge(startNode, nextNode, distance);
+                    g.addEdge(e);
+                }
+            }
+        }
+        reader.close();
+        return g;
+    }
+    
     public int nodeCount()
     {
         return nodes.size();
@@ -79,7 +174,7 @@ public class Graph
         {
             edges += entry.getValue().size();
         }
-        return edges;
+        return edges / 2;
     }
     
     public static Graph generateRandomGraph(int nodes)
@@ -93,7 +188,7 @@ public class Graph
         Random rand = new Random();
         ArrayList<Integer> nodesArray = new ArrayList<Integer>(g.nodes);
         HashSet<String> edges = new HashSet<>();
-        for (int i=0; i<nodes*2; i++)
+        for (int i=0; i<nodes*10; i++)
         {
             int pos1 = rand.nextInt(nodes);
             int pos2 = rand.nextInt(nodes-1);
@@ -183,11 +278,19 @@ public class Graph
         System.out.println("Number of edges = " + g.edgeCount());
     }
     
+    public static void genRandomGraph(int nodes, String fileName) throws IOException
+    {
+        Graph g = generateRandomGraph(nodes);
+        g.print(fileName);
+        System.out.println("Number of nodes = " + g.nodeCount());
+        System.out.println("Number of edges = " + g.edgeCount());
+    }
+
     public static void main(String[] args) throws Exception
     {
         // test01("C:/Users/rubandyopadhyay/Downloads/dijkstraData.txt");
         // test01("data/shortest_path_01.txt");
-        genRandomGraph(30);
+        genRandomGraph(10000, "C:/Users/rubandyopadhyay/Downloads/largeGraph.txt");
     }
 
 }
