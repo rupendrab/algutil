6434231fed1b269dc4d7a0edff0965d37908ab1d
Rupendra Bandyopadhyay <rbandyo@yahoo.com>
2017-01-06 16:30:20 -0500

    Create SortNumberFile.cpp

---
+++ cplusplus/heapgeneric/SortNumberFile.cpp
@@ -0,0 +1,39 @@
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "MinHeap.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    if (argc != 3)
+    {
+        cout << "Usage: " << argv[0] << " <Input File name> <Output File Name>" << endl;
+        return 1;
+    }
+    MinHeap<int>* mh = new MinHeap<int>::MinHeap();
+    ifstream infile;
+    string line;
+    infile.open(argv[1], std::ifstream::in);
+    int i = 0;
+    while (getline(infile, line))
+    {
+        int val = stoi(line);
+        mh->insert(val);
+        i++;
+    }
+    infile.close();
+
+    ofstream outfile;
+    outfile.open(argv[2]);
+    while (mh->getSize() > 0)
+    {
+        outfile << mh->extractMin() << endl;
+    }
+    outfile.close();
+
+    return 0;
+}
