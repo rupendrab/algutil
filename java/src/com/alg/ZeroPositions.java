180945628d0cc5019e5aa7545ae4e29db74c0018
Rupendra Bandyopadhyay <rbandyo@yahoo.com>
2017-01-13 15:29:54 -0500

    Create ZeroPositions.java

---
+++ java/src/com/alg/ZeroPositions.java
@@ -0,0 +1,73 @@
+package com.alg;
+
+import java.util.TreeSet;
+
+public class ZeroPositions implements Comparable<ZeroPositions>
+{
+    Integer index;
+    TreeSet<Integer> subIndices = new TreeSet<>();
+    
+    public ZeroPositions(Integer index)
+    {
+        super();
+        this.index = index;
+    }
+    
+    public boolean addZeroPosition(int subposition)
+    {
+        return subIndices.add(subposition);
+    }
+    
+    public boolean deleteZeroPosition(int subposition)
+    {
+        return subIndices.remove(subposition);
+    }
+
+    public int getSize()
+    {
+        return subIndices.size();
+    }
+    
+    public Integer getIndex()
+    {
+        return index;
+    }
+
+    public TreeSet<Integer> getSubIndices()
+    {
+        return subIndices;
+    }
+
+    @Override
+    public int compareTo(ZeroPositions o)
+    {
+        return getSize() - o.getSize();
+    }
+    
+    @Override
+    public boolean equals(Object o)
+    {
+        if (o == null)
+        {
+            return false;
+        }
+        if (! (o instanceof ZeroPositions))
+        {
+            return false;
+        }
+        ZeroPositions other = (ZeroPositions) o;
+        return getIndex() == other.getIndex(); 
+    }
+    
+    @Override
+    public int hashCode()
+    {
+        return index.hashCode();
+    }
+    
+    public String toString()
+    {
+        return index + " (" + getSize() + "): " + subIndices;
+    }
+
+}
