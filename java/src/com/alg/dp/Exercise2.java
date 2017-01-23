c420814e65dd3196d80147725062da1407d45615
Rupendra Bandyopadhyay <rbandyo@yahoo.com>
2017-01-23 16:39:16 -0500

    Update Exercise2.java

--- java/src/com/alg/dp/Exercise2.java
+++ java/src/com/alg/dp/Exercise2.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Calendar;
 
 public class Exercise2
 {
@@ -19,12 +20,22 @@ public class Exercise2
         this.fileName = fileName;
         loadFile();
         ks = new Knapsack(weights, values);
+    }
+    
+    public void solve()
+    {
         ks.solve(W);
     }
     
     public double optimalSolution()
     {
         return ks.optimalSolution(W);
+    }
+    
+    public int callDetermine()
+    {
+        ks.determineNeededSolutions(W);
+        return ks.toSolve.getSize();
     }
     
     public void loadFile() throws IOException
@@ -52,11 +63,53 @@ public class Exercise2
         reader.close();
     }
     
-    public static void main(String[] args) throws Exception
+    public void sortByDecreasingWeight()
     {
-        Exercise2 ex2 = new Exercise2("/Users/rupen/Documents/Coursera/Algorithms/Course3/knapsack1.txt");
+        ks.sortByDecreasingWeight();
+    }
+    
+    public Double solveOptimal()
+    {
+        return ks.solveOptimal(W);
+    }
+    
+    public static void test01(String fileName) throws IOException
+    {
+        // Exercise2 ex2 = new Exercise2("/Users/rupen/Documents/Coursera/Algorithms/Course3/knapsack1.txt");
+        Exercise2 ex2 = new Exercise2(fileName);
+        ex2.solve();
+        ex2.ks.print();
+        System.out.println(ex2.ks.selectedPoints(ex2.W));
         System.out.println(ex2.optimalSolution());
         /* 2493893.0 */
+    }
+    
+    public static long time()
+    {
+        return Calendar.getInstance().getTimeInMillis();
+    }
+    
+    public static void test02(String fileName) throws IOException
+    {
+        long start, end = 0;
+        start = time();
+        Exercise2 ex2 = new Exercise2(fileName);
+        ex2.sortByDecreasingWeight();
+        System.out.println(ex2.solveOptimal());
+        end = time();
+        System.out.println(String.format("Time to solve = %d ms", (end - start)));
+        System.out.println("Size of hash table = " + ex2.ks.alreadySolved.size());
+        /* 4243395.0 */
+    }
+    
+    public static void main(String[] args) throws Exception
+    {
+        // test01("C:\\Users\\rubandyopadhyay\\Downloads\\knapsack1.txt");
+        test02("C:\\Users\\rubandyopadhyay\\Downloads\\knapsack_big.txt");
+        // test01("C:\\Users\\rubandyopadhyay\\Downloads\\knapsack_test_03.txt");
+        // test02("C:\\Users\\rubandyopadhyay\\Downloads\\knapsack_test_03.txt");
+        // test01("C:\\Users\\rubandyopadhyay\\Downloads\\knapsack_test_02.txt");
+        // test02("C:\\Users\\rubandyopadhyay\\Downloads\\knapsack1.txt");
     }
 
 }
